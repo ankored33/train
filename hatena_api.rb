1ba10885e10b3b11c684cc8aafdb16725d00bb3e
Masato Doi <bridgehead33@gmail.com>
2016-12-14 00:18:06 +0000

    stardata to hash

--- hatena_api.rb
+++ hatena_api.rb
@@ -2,10 +2,12 @@ require 'open-uri'
 require 'json'
 
 
+
+  opt = {}
+  opt['User-Agent'] = 'Opera/9.80 (Windows NT 5.1; U; ja) Presto/2.7.62 Version/11.01 ' #User-Agent偽装
+
 #はてなAPIから返るjsonをハッシュ化 
 uri = 'http://b.hatena.ne.jp/entry/json/?url=http://anond.hatelabo.jp/20161206134710' #URLを必要に応じて変更
-opt = {}
-opt['User-Agent'] = 'Opera/9.80 (Windows NT 5.1; U; ja) Presto/2.7.62 Version/11.01 ' #User-Agent偽装
   io = open(uri, opt)
   hash = JSON.load(io)
   bkm = hash["bookmarks"]
@@ -14,10 +16,6 @@ opt['User-Agent'] = 'Opera/9.80 (Windows NT 5.1; U; ja) Presto/2.7.62 Version/11
 for var in bkm do
     timestamp = var["timestamp"].delete("/")[0,8].to_s
     user = var["user"]
-
-#   53の["entries"]が空の人の処理を考えないといけない
-#   timestamp = bkm[53]["timestamp"].delete("/")[0,8].to_s
-#   user = bkm[53]["user"]
 
     #ブコメパーマリンク生成
     starurl = "http://s.hatena.com/entry.json?uri=http://b.hatena.ne.jp/#{user}/#{timestamp}#bookmark-#{eid}"
@@ -30,7 +28,7 @@ for var in bkm do
 
   #["colored_stars"]を探索して色ごとに配列に突っ込む
   colorstars = Array.new
-  if hash_s["entries"].empty? == false #53の人用のif文
+  if hash_s["entries"].empty? == false #53のような["entries"]が空の人用のif文
     x = hash_s["entries"][0]["colored_stars"]
     if x != nil
       for var_s in x do
@@ -39,21 +37,30 @@ for var in bkm do
       end
     else
     end
-    y_star = hash_s["entries"][0]["stars"].length #黄色スターの数
-    g_star = colorstars.count("green").to_i
-    r_star = colorstars.count("red").to_i
-    b_star = colorstars.count("blue").to_i
-    p_star = colorstars.count("purple").to_i
+      y_star = hash_s["entries"][0]["stars"].length #黄色スターの数
+      g_star = colorstars.count("green").to_i
+      r_star = colorstars.count("red").to_i
+      b_star = colorstars.count("blue").to_i
+      p_star = colorstars.count("purple").to_i
+      s_power = y_star + g_star * 5 + r_star * 20 + b_star * 50 + p_star * 100
   else
-    y_star = 0 #["entries"]が空の人はとりあえず全部0
-    g_star = 0
-    r_star = 0
-    b_star = 0
-    p_star = 0
+      y_star = 0 #["entries"]が空の人はとりあえず全部0
+      g_star = 0
+      r_star = 0
+      b_star = 0
+      p_star = 0
+      s_power = 0
   end
-    p user + ", y" + y_star.to_s + ", g" + g_star.to_s + ", r" + r_star.to_s + ", b" + b_star.to_s + ", p" + p_star.to_s
-
+    #p user + ", y" + y_star.to_s + ", g" + g_star.to_s + ", r" + r_star.to_s + ", b" + b_star.to_s + ", p" + p_star.to_s
+    #p user + " #{s_power}"
+  var["y_star"] = y_star
+  var["g_star"] = g_star
+  var["r_star"] = r_star
+  var["b_star"] = b_star
+  var["p_star"] = p_star
+  var["s_power"] = s_power
 end #for文のend
+p bkm
 
 #ブコメのスター数を返すAPI
 #http://s.hatena.com/entry.json?uri={ブコメのパーマリンク}
@@ -62,3 +69,7 @@ end #for文のend
 #http://b.hatena.ne.jp/{ユーザーID}/{コメントの日付(YYYYMMDD)}#bookmark-{エントリーID}
 
 #URI.escape(url)
+
+
+#次やること
+#def をつかう
