ccefc8426b9030b05e6a36425263a85eb241ceda
Masato Doi <bridgehead33@gmail.com>
2016-12-15 07:21:21 +0000

    pre hatena in

--- app.rb
+++ app.rb
@@ -5,9 +5,9 @@ get '/' do
   slim :index
 end
 
-get '/member/:name' do
+get '/geturl/:url' do
   member_age = rand(100)
   content_type :json
-  data = {name: params[:name], age: member_age}
+  data = {url: params[:url], age: member_age}
   data.to_json
 end
--- public/main.js
+++ public/main.js
@@ -1,12 +1,12 @@
 $(function(){
   $('#load').click(function(){
-    var member_name = $('#member_name').val();
+    var member_name = $('#url').val();
     $.ajax({
       type: 'GET',
-      url: 'https://shooting-barbieri0303.c9users.io/member/' + member_name,
+      url: 'https://shooting-barbieri0303.c9users.io/geturl/' + member_name,
       dataType: 'json',
       success: function(json) {
-        $('#result').append('<ul><li>' + json.name + '</li><li>' + json.age + '</li></ul>');
+        $('#result').append('<ul><li>' + json.url + '</li><li>' + json.age + '</li></ul>');
       },
       error: function() {
         $('#result').append('error');
