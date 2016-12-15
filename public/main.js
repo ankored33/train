b9cf60702b41a65ec5c3ac3c7b2ca77bc0cb4a8a
Masato Doi <bridgehead33@gmail.com>
2016-12-15 12:03:13 +0000

    url data post

--- public/main.js
+++ public/main.js
@@ -1,9 +1,9 @@
 $(function(){
   $('#load').click(function(){
-    var member_name = $('#url').val();
+    var geturl = $('#url').val();
     $.ajax({
       type: 'GET',
-      url: 'https://shooting-barbieri0303.c9users.io/geturl/' + member_name,
+      url: 'https://shooting-barbieri0303.c9users.io/geturl/',
       dataType: 'json',
       success: function(json) {
         $('#result').append('<ul><li>' + json.url + '</li><li>' + json.age + '</li></ul>');
